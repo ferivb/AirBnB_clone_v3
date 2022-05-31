c77418681a4923cd10ab12ec8f822a1d5743aaa1
Felipe Rivas <fe.rivasb@gmail.com>
2022-05-31 14:40:17 -0500

    task 7 update api/v1/views/states -> added comments

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,6 +13,7 @@ def getAllStates():
     """Retrieves the list of all State objects"""
     states = []
     for state in storage.all("State").values():
+        # Using to_dict to retrieve an object into a valid JSON
         states.append(state.to_dict())
 
     return jsonify(states)
@@ -44,7 +45,9 @@ def deleteState(state_id=None):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def postState():
     """Create a state"""
+    # Parses the incoming JSON request data and returns it as a dict
     dict = request.get_json()
+    # dict will == None if the mimetype is not application/json
     if dict is None:
         abort(400, "Not a JSON")
     elif "name" not in dict.keys():
@@ -59,10 +62,13 @@ def postState():
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def putState(state_id=None):
     """Updates a State object"""
+    # Retireve the state from storage using state_id
     state = storage.get(State, state_id)
     if not state:
         abort(404)
+    # Parses the incoming JSON request data and returns it as a dict
     dict = request.get_json()
+    # dict will == None if the mimetype is not application/json
     if dict is None:
         abort(400, "Not a JSON")
     else:

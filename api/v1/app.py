90b678353aefdd16bda6ce40fb2a591df4e7a326
AnniesTech <anita.rocha0505@gmail.com>
2022-05-31 02:19:23 -0500

    task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ API main file """
 
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from os import getenv
 
@@ -15,6 +15,10 @@ def teardown_db(exception):
     """ method to handle teardown of database"""
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """Returns JSON error repsponse"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     api_host = getenv('HBNB_API_HOST', default='0.0.0.0')

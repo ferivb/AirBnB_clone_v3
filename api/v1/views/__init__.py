758a24bc2d85f006ab7d4f1a45e12cff0515109a
AnniesTech <anita.rocha0505@gmail.com>
2022-05-31 03:29:16 -0500

    got more checkers bad, trying to go back to old version

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""Views module init"""
+"""
+views module init
+"""
+
+
 from flask import Blueprint
+from api.v1.views import *
 from api.v1.views.index import *
 from api.v1.views.states import *
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,8 +3,8 @@
 States view for API
 """
 from api.v1.views import app_views
-from flask import request, jsonify, abort, make_response
-from models import storage, State
+from flask import request, jsonify, abort
+from models import storage, state
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -42,14 +42,16 @@ def delete_state(state_id=None):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """creates a new state"""
-    if not request.get_json:
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if "name" not in request.get_json():
-        return make_response(jsonify({'error': 'Missing name'}), 400)
+    s = request.get_json(silent=True)
+    if s is None:
+        abort(400, "Not a JSON")
+    elif "name" not in s.keys():
+        abort(400, "Missing name")
     else:
-        state = State(**request.get_json())
-        state.save()
-        return make_response(jsonify(state.to_dict()), 201)
+        new_s = state.State(**s)
+        storage.new(new_s)
+        storage.save()
+        return jsonify(new_s.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
@@ -62,10 +64,12 @@ def updatestate(state_id=None):
     s = request.get_json(silent=True)
     if s is None:
         abort(400, "Not a JSON")
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for key, value in request.get_json().items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(obj, key, value)
-        obj.save()
-        return jsonify(obj.to_dict), 200
+    else:
+        for k, v in s.items():
+            if k in ['id', 'created_at', 'updated_at']:
+                pass
+            else:
+                setattr(obj, k, v)
+        storage.save()
+        res = obj.to_dict()
+        return jsonify(res), 200

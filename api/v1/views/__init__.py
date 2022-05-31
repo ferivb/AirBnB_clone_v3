39f08bd0b390f0e2569afd650179526437742f2e
AnniesTech <anita.rocha0505@gmail.com>
2022-05-31 03:48:27 -0500

    deleted all for task 7

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""
-views module init
-"""
-
-
+"""Views module init"""
 from flask import Blueprint
-from api.v1.views import *
-from api.v1.views.index import *
-from api.v1.views.states import *
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *

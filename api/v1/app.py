3f9e4612d29bc2e6b7c6130ef96e231d88bcacb0
Felipe Rivas <fe.rivasb@gmail.com>
2022-05-30 22:32:34 -0500

    task 4 added the api dir + init for v1, views and api

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" API main file """
+
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import storage
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ method to handle teardown of database"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    api_port = getenv('HBNB_API_PORT', default=5000)
+    app.run(host=api_host, port=int(api_port), threaded=True, debug=True)

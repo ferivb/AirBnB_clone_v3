7bd407020457dc0e5b0a06f1baa5a03140c2b53f
AnniesTech <anita.rocha0505@gmail.com>
2022-05-31 19:17:08 -0500

    task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,10 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from models import storage
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, orgins='0.0.0.0')
 app.register_blueprint(app_views)
 
 
